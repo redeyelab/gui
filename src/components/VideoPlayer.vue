c8212695c0736bc4c736faa47fbe3ed4613ef39c
Rusty Eddy <rusty@rustyeddy.com>
2020-04-09 10:46:50 -0700

    properly respondng to play and pause events

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,12 +1,34 @@
 <template>
     <div id="video-player">
         <img width="100%" id="video" class="video mb-4" src="http://10.24.10.10:8887/mjpeg" alt="streaming video" />
-        <button id="play" type="button" class="btn btn-primary btn-lg btn-block">Play</button>
-        <button id="pause" type="button" class="btn btn-secondary btn-lg btn-block">Pause</button>
+        <button v-on:click="play" id="play" type="button" class="btn btn-primary btn-lg btn-block">Play</button>
+        <button v-on:click="pause" id="pause" type="button" class="btn btn-secondary btn-lg btn-block">Pause</button>
     </div>
 </template>
 <script>
- 
+ module.exports = {
+     
+     methods: {
+         play: function() {
+             var sendmsg = {
+                 t: "video",
+                 l: 1,
+                 v: "play"
+             }
+             console.log(sendmsg);
+             // ws.send(JSON.stringify(sendmsg));
+         },
+         pause: function() {
+             var sendmsg = {
+                 t: "video",
+                 l: 1,
+                 v: "pause"
+             }
+             console.log(sendmsg);
+             // ws.send(JSON.stringify(sendmsg));
+         }
+     }
+ };     
 </script>
 <style>
 
